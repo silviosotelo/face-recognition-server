c1e5cde2ff531456a21c5c72313235884c90e538
Silvio Sotelo <40511743+silviosotelo@users.noreply.github.com>
2024-06-27 14:03:52 -0400

    Update database.js

--- database.js
+++ database.js
@@ -4,9 +4,7 @@ const db = new sqlite3.Database('./database.sqlite');
 db.serialize(() => {
   db.run(`CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    id_cliente TEXT,
     name TEXT,
-    ci TEXT,
     descriptor TEXT
   )`);
 });
